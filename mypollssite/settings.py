a3afb43d3bb08cc58ad53d431669d31cb9e94804
allania7med11 <allania7med11@gmail.com>
2021-08-12 13:17:01 +0100

    part 2

--- mypollssite/settings.py
+++ mypollssite/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+TIME_ZONE = "Africa/Algiers"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
