fc246322c49cb1ae06b8c4f2a038b4b4b52f0554
allania7med11 <allania7med11@gmail.com>
2021-08-12 14:58:53 +0100

    polls detail(Declarative way)

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.http.response import Http404
 from polls.models import Question
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
 def index(request):
@@ -8,11 +8,9 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
+
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
